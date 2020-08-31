55a7a02197d7f53d398d117b145c92964d27768a
Yauhen Yakimovich <eugeny.yakimovitch@gmail.com>
2020-08-31 21:23:42 +0200

    Change path

--- publish.sh
+++ publish.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 SRC=../bsnl-wallet
 echo 'Publishing wallet app..'
-if [[ -d wallet ]]; then rm -rf wallet/; fi
-if [[ ! -d wallet ]]; then mkdir wallet; fi
 # Assume the app was already build, committed and git-signed..
-cp -R ${SRC}/build/* wallet/
+cp -R ${SRC}/build/* ./
