2129565c3bf72189600b0096107d824de0eb2656
Yauhen Yakimovich <eugeny.yakimovitch@gmail.com>
2020-08-31 23:04:20 +0200

    Add token list

--- publish.sh
+++ publish.sh
@@ -3,3 +3,5 @@ SRC=../bsnl-wallet
 echo 'Publishing wallet app..'
 # Assume the app was already build, committed and git-signed..
 cp -R ${SRC}/build/* ./
+# Add token list  https://github.com/Uniswap/token-lists
+cp ${SRC}/extra/snail.tokenlist.json ./
